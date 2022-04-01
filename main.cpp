52bf001405de9e2b316f28a211bdff95935c36fa
nwelter1 <nwelter1@gmail.com>
2022-04-01 00:07:04 -0500

    first cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  something_new
+//
+//  Created by Nathan Welter on 3/31/22.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    // insert code here...
+    cout << "May I know your name???";
+    cin >> name;
+    cout << "You don't know shit about static typing Mr/Mrs " << name + "\n";
+    return 0;
+}
